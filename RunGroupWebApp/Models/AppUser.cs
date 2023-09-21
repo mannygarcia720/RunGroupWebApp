8ac76c3bbe7bcc8bb3a149b7a59d743113dce083
mannygarcia720 <37597363+mannygarcia720@users.noreply.github.com>
2023-09-20 17:01:31 -0700

    seeded database & created views

--- RunGroupWebApp/Models/AppUser.cs
+++ RunGroupWebApp/Models/AppUser.cs
@@ -3,6 +3,8 @@ namespace RunGroupWebApp.Models
 {
     public class AppUser
     {
+        [Key] 
+        public string Id { get; set; }
         public Address? Address { get; set; }
 
         public int? Pace { get; set; }
